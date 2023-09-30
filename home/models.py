5f79d22559c2090fc8b04a213c2071bc99a8563c
PrabhanshuGithub <104717131+PrabhanshuGithub@users.noreply.github.com>
2023-10-01 03:55:45 +0530

    Update models.py
    roll backed the changes made by Nitish Ji

--- home/models.py
+++ home/models.py
@@ -8,29 +8,10 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=20)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=128)  # Use a CharField with enough length for hashed passwords
-    # createdDate = models.DateTimeField(auto_now=True, null=True, default = timezone.now)
+    createdDate = models.DateTimeField(auto_now=True, null=True, default = timezone.now)
   
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-    
-
-
-TICKET_STATUS_CHOICES = [
-    ('Awaiting', 'Awaiting'),
-    ('Pending', 'Pending'),
-    ('Closed', 'Closed'),
-]
-
 
 class Ticket(models.Model):
     fullname = models.CharField(max_length=20)
     email = models.ForeignKey(Contact, on_delete=models.CASCADE)
-    # createdDate = models.DateTimeField(auto_now=True, null=True, default = timezone.now)
- 
-    concern = models.CharField(max_length=700, default= "")
-    status = models.CharField(max_length=10, choices=TICKET_STATUS_CHOICES, default= "Awaiting")
-
-    def __str__(self):
-        return self.fullname
+    createdDate = models.DateTimeField(auto_now=True, null=True, default = timezone.now)
